2bef436497d8f14ee1ed2ac2b4811cfc5f0be070
Saman Pordanesh <sina.pordanesh@yahoo.com>
2023-06-28 16:31:12 -0600

    Build a basic client using go

---
+++ sshclient/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/codered-by-ec-council/Hands-on-Network-Programming-with-Go/pkg/devcon"
+)
+
+func main() {
+	client, err:= devcon.NewClient(
+		os.Getenv("SSH_USER"),
+		"localhost",
+		devcon.SetPassword(os.Getenv("SSH_PASSWORD")), 
+		devcon.SetTimeout(time.Second*5),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	output, err := client.Run("show version")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(output)
+}
